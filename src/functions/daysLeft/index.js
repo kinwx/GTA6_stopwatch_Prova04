167324ab7c80fa6444f9cc63bfe707977168f589
kinwx <129900138+kinwx@users.noreply.github.com>
2023-12-08 00:56:18 -0300

    Add files via upload

--- src/functions/daysLeft/index.js
+++ src/functions/daysLeft/index.js
@@ -2,8 +2,7 @@ import { currentMonth } from "../currentMonth";
 
 const daysLeft = () => {
   const currentDay = new Date().getDate();
-  // por favor, desconsidera que próx ano é bissexto 😔🙏🏽
-  if (currentMonth() === 2) return 28 - currentDay;
+  if (currentMonth() === 2) return 29 - currentDay;
   
   if ([2, 4, 6, 9, 11].includes(currentMonth())) {
     return 30 - currentDay;
